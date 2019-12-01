9bbe23e3b962ab85a9137514937f0af970c10165
bloomkail <arnaud.lambert@outlook.com>
2019-12-01 17:10:39 +0100

    less header in main

--- main.cpp
+++ main.cpp
@@ -1,13 +1,4 @@
 #include "src/scene/scene.h"
-#include "../geometrie/rectangle_translatable.h"
-#include "../geometrie/losange_translatable.h"
-#include "../geometrie/disk_translatable.h"
-#include "../geometrie/triangle_translatable.h"
-
-#include "../geometrie/rectangle_rotatable.h"
-#include "../geometrie/losange_rotatable.h"
-#include "../geometrie/disk_rotatable.h"
-#include "../geometrie/triangle_rotatable.h"
 
 int main()
 {
